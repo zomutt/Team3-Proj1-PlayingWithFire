bd50a3fa957282c4b84ed89049e8925e78b6ea69
zomutt <christiejcomer@gmail.com>
2026-08-22 17:01:53 -0400

    Beginning Raycast/Fire mechanic

---
+++ Team3-Proj1-PlayingWithFire/Assets/1A_Scripts/PlayerFire.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+/// <summary>
+/// Began by: Christie Comer 5:00pm 08/22/2026
+/// 
+/// This is a small script that will handle the use of the princess' fire spell.
+/// NOTE: THE FIRE IS NOT AN ATTACK.
+/// Considering the enemies are water, fire is her tool and her escape, not her weapon.
+/// </summary>
+public class PlayerFire : MonoBehaviour
+{ 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
