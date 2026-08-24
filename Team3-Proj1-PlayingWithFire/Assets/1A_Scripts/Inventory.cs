18e46f08adcd6a04901f9e5bbe76813037372d7d
zomutt <christiejcomer@gmail.com>
2026-08-24 13:54:46 -0400

    Revert "i fixed the repo bug??"
    This reverts commit 653c96092d3ce7b3266b9d72f2be6cdafaffea0b.

--- Team3-Proj1-PlayingWithFire/Assets/1A_Scripts/Inventory.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-/// <summary>
-/// The player will have an inventory for things like objective weapons and others. This class handles storing what the player currently has.
-/// </summary>
-public class Inventory : MonoBehaviour
-{
-
-}
